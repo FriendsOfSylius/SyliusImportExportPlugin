404b78bf611b423bf274733c39a9ed4b8d4575fa
enesaktay <enaktay@htwg-konstanz.de>
2018-05-25 11:13:47 +0200

    refactored a big foreach to make it more understandable

--- src/Exporter/Plugin/OrderResourcePlugin.php
+++ src/Exporter/Plugin/OrderResourcePlugin.php
@@ -26,65 +26,110 @@ class OrderResourcePlugin extends ResourcePlugin
         foreach ($this->resources as $resource) {
             // insert customer information to specific fields
             if ($resource->getCustomer()) {
-                $customer = $resource->getCustomer();
-                $this->addDataForResource($resource, 'Gender', $customer->getGender() ? $customer->getGender() : '');
-                $this->addDataForResource($resource, 'Full_name', $customer->getFullName() ? $customer->getFullName() : '');
-                $this->addDataForResource($resource, 'Telephone', $customer->getPhoneNumber() ? $customer->getPhoneNumber() : '');
-                $this->addDataForResource($resource, 'Email', $customer->getEmail() ? $customer->getEmail() : '');
+                $this->addCustomerData($resource);
             }
 
             // insert shippingaddress to the specific field
             if ($resource->getShippingAddress()) {
-                $shippingAddress = $resource->getShippingAddress();
-                $this->addDataForResource($resource, 'Shipping_address', sprintf(
-                    '%s, %s, %s, %s, %s',
-                    $shippingAddress->getFullName() ? $shippingAddress->getFullName() : '',
-                    $shippingAddress->getStreet() ? $shippingAddress->getStreet() : '',
-                    $shippingAddress->getCity() ? $shippingAddress->getCity() : '',
-                    $shippingAddress->getPostcode() ? $shippingAddress->getPostcode() : '',
-                    $shippingAddress->getCountryCode() ? $shippingAddress->getCountryCode() : ''
-                ));
+                $this->addShippingAddressData($resource);
             }
 
             // insert billingaddress to the specific field
             if ($resource->getBillingAddress()) {
-                $billingAddress = $resource->getBillingAddress();
-                $this->addDataForResource($resource, 'Billing_address', sprintf(
-                    '%s, %s, %s, %s, %s',
-                    $billingAddress->getFullName() ? $billingAddress->getFullName() : '',
-                    $billingAddress->getStreet() ? $billingAddress->getStreet() : '',
-                    $billingAddress->getCity() ? $billingAddress->getCity() : '',
-                    $billingAddress->getPostcode() ? $billingAddress->getPostcode() : '',
-                    $billingAddress->getCountryCode() ? $billingAddress->getCountryCode() : ''
-                ));
+                $this->addBillingAddressData($resource);
             }
 
-            $items = [];
+            $items = $this->getItemsAndCount($resource);
 
-            /** @var OrderItemInterface $orderItem */
-            foreach ($resource->getItems() as $orderItem) {
-                /** @var ProductVariantInterface $variant */
-                $variant = $orderItem->getVariant();
-                /** @var ProductInterface $product */
-                $product = $variant->getProduct();
-
-                if (!isset($items[$product->getName()])) {
-                    $items[$product->getName()] = 0;
-                }
-
-                $items[$product->getName()] += $orderItem->getQuantity();
-            }
-
-            $str = '';
-
-            foreach ($items as $itemName => $quantity) {
-                if (!empty($str)) {
-                    $str .= ' | ';
-                }
-                $str .= sprintf('%dx %s', $quantity, $itemName);
-            }
-
-            $this->addDataForResource($resource, 'Product_list', $str);
+            $this->addOrderItemData($items, $resource);
         }
     }
+
+    /**
+     * @param OrderInterface $resource
+     */
+    private function addCustomerData(OrderInterface $resource)
+    {
+        $customer = $resource->getCustomer();
+        $this->addDataForResource($resource, 'Gender', $customer->getGender() ? $customer->getGender() : '');
+        $this->addDataForResource($resource, 'Full_name', $customer->getFullName() ? $customer->getFullName() : '');
+        $this->addDataForResource($resource, 'Telephone', $customer->getPhoneNumber() ? $customer->getPhoneNumber() : '');
+        $this->addDataForResource($resource, 'Email', $customer->getEmail() ? $customer->getEmail() : '');
+    }
+
+    /**
+     * @param OrderInterface $resource
+     */
+    private function addShippingAddressData(OrderInterface $resource)
+    {
+        $shippingAddress = $resource->getShippingAddress();
+        $this->addDataForResource($resource, 'Shipping_address', sprintf(
+            '%s, %s, %s, %s, %s',
+            $shippingAddress->getFullName() ? $shippingAddress->getFullName() : '',
+            $shippingAddress->getStreet() ? $shippingAddress->getStreet() : '',
+            $shippingAddress->getCity() ? $shippingAddress->getCity() : '',
+            $shippingAddress->getPostcode() ? $shippingAddress->getPostcode() : '',
+            $shippingAddress->getCountryCode() ? $shippingAddress->getCountryCode() : ''
+        ));
+    }
+
+    /**
+     * @param OrderInterface $resource
+     */
+    private function addBillingAddressData(OrderInterface $resource)
+    {
+        $billingAddress = $resource->getBillingAddress();
+        $this->addDataForResource($resource, 'Billing_address', sprintf(
+            '%s, %s, %s, %s, %s',
+            $billingAddress->getFullName() ? $billingAddress->getFullName() : '',
+            $billingAddress->getStreet() ? $billingAddress->getStreet() : '',
+            $billingAddress->getCity() ? $billingAddress->getCity() : '',
+            $billingAddress->getPostcode() ? $billingAddress->getPostcode() : '',
+            $billingAddress->getCountryCode() ? $billingAddress->getCountryCode() : ''
+        ));
+    }
+
+    /**
+     * @param OrderInterface $resource
+     *
+     * @return array
+     */
+    private function getItemsAndCount(OrderInterface $resource)
+    {
+        $items = [];
+
+        /** @var OrderItemInterface $orderItem */
+        foreach ($resource->getItems() as $orderItem) {
+            /** @var ProductVariantInterface $variant */
+            $variant = $orderItem->getVariant();
+            /** @var ProductInterface $product */
+            $product = $variant->getProduct();
+
+            if (!isset($items[$product->getName()])) {
+                $items[$product->getName()] = 0;
+            }
+
+            $items[$product->getName()] += $orderItem->getQuantity();
+        }
+
+        return $items;
+    }
+
+    /**
+     * @param OrderInterface $resource
+     */
+    private function addOrderItemData(array $items, OrderInterface $resource)
+    {
+        $str = '';
+
+        foreach ($items as $itemName => $quantity) {
+            if (!empty($str)) {
+                $str .= ' | ';
+            }
+            $str .= sprintf('%dx %s', $quantity, $itemName);
+        }
+
+        $this->addDataForResource($resource, 'Product_list', $str);
+    }
+
 }
