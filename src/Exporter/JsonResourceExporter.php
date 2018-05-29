33f121d5ae452e54ba91e22b615e7ea8bd10c4bb
enesaktay <enaktay@htwg-konstanz.de>
2018-05-29 13:43:06 +0200

    created a Json Exporter extending resourceexporter

---
+++ src/Exporter/JsonResourceExporter.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FriendsOfSylius\SyliusImportExportPlugin\Exporter;
+
+/**
+ * Class JsonResourceExporter
+ */
+class JsonResourceExporter extends ResourceExporter
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function export(array $idsToExport): void
+    {
+        $this->pluginPool->initPlugins($idsToExport);
+
+        foreach ($idsToExport as $id) {
+            $this->writeDataForId((string) $id);
+        }
+    }
+
+    /**
+     * @param string $id
+     */
+    protected function writeDataForId(string $id): void
+    {
+        $dataForId = $this->getDataForId($id);
+
+        // filter out only resourcekeys
+        foreach ($dataForId as $dataName => $data) {
+            if (!in_array($dataName, $this->resourceKeys)) {
+                unset($dataForId[$dataName]);
+            }
+        }
+
+        $this->writer->write($dataForId);
+    }
+
+}
