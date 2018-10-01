ad5dea6219cdce1c2c711306f4bc0c004b39a849
Sylvain Pras <sylvain.pras@jetpulp.fr>
2018-10-01 11:11:07 +0200

    Fix phpspec taxonomies

--- spec/Processor/TaxonomyProcessorSpec.php
+++ spec/Processor/TaxonomyProcessorSpec.php
@@ -9,7 +9,7 @@ use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Sylius\Component\Locale\Provider\LocaleProviderInterface;
 use Sylius\Component\Taxonomy\Factory\TaxonFactoryInterface;
-use Sylius\Component\Taxonomy\Model\TaxonInterface;
+use Sylius\Component\Core\Model\TaxonInterface;
 use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
 
 class TaxonomyProcessorSpec extends ObjectBehavior
@@ -41,12 +41,22 @@ class TaxonomyProcessorSpec extends ObjectBehavior
         MetadataValidatorInterface $metadataValidator
     ) {
         $headerKeys = ['Code','Parent','Locale','Name','Slug','Description'];
-        $dataset = ['Code' => 'root', 'Parent' => '', 'Locale' => 'en_US', 'Name' => 'Root', 'root', 'Description' => 'root category'];
+        $dataset = ['Code' => 'root', 'Parent' => '', 'Locale' => 'en_US', 'Name' => 'Root', 'Slug' => 'root', 'Description' => 'root category'];
         $this->beConstructedWith($taxonFactory, $taxonRepository, $localeProviderInterface, $metadataValidator, $headerKeys);
 
-        $taxonFactory->findOneBy(['code' => 'root'])->willReturn(null);
-        $taxonFactory->add($taxon)->shouldBeCalledTimes(1);
+        $taxonRepository->findOneBy(['code' => ''])->willReturn(null);
+        $taxonRepository->findOneBy(['code' => 'root'])->willReturn(null);
+        $taxonRepository->add($taxon)->shouldBeCalledTimes(1);
         $taxonFactory->createNew()->willReturn($taxon);
+
+        $localeProviderInterface->getAvailableLocalesCodes()->willReturn(['en_US']);
+
+        $taxon->setCurrentLocale('en_US')->shouldBeCalled();
+        $taxon->setFallbackLocale('en_US')->shouldBeCalled();
+        $taxon->setCode('root')->shouldBeCalled();
+        $taxon->setName('Root')->shouldBeCalled();
+        $taxon->setSlug('root')->shouldBeCalled();
+        $taxon->setDescription('root category')->shouldBeCalled();
 
         $this->process($dataset);
     }
