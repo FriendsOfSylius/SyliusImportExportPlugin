8cfb538432e66beadac7cabb3019eb1c10bd3ce2
oallain <oallain@users.noreply.github.com>
2020-01-22 22:42:11 +0100

    move in function

--- src/Exporter/Plugin/OrderResourcePlugin.php
+++ src/Exporter/Plugin/OrderResourcePlugin.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use FriendsOfSylius\SyliusImportExportPlugin\Exporter\ORM\Hydrator\HydratorInterface;
 use FriendsOfSylius\SyliusImportExportPlugin\Service\AddressConcatenationInterface;
 use Sylius\Component\Core\Model\OrderInterface;
-use Sylius\Component\Order\Model\OrderInterface as OrderInterfaceB;
 use Sylius\Component\Core\Model\OrderItemInterface;
 use Sylius\Component\Product\Model\ProductInterface;
 use Sylius\Component\Product\Model\ProductVariantInterface;
@@ -46,9 +45,11 @@ class OrderResourcePlugin extends ResourcePlugin
 
         /** @var OrderInterface $resource */
         foreach ($this->resources as $resource) {
+            // insert general fields 
+            $this->addGeneralData($resource);
+            
             // insert customer information to specific fields
             $this->addCustomerData($resource);
-            
 
             // insert shippingaddress to the specific field
             $this->addShippingAddressData($resource);
@@ -59,20 +60,19 @@ class OrderResourcePlugin extends ResourcePlugin
             $items = $this->getItemsAndCount($resource);
 
             $this->addOrderItemData($items, $resource);
-            $this->addDataForResource($resource, 'Currency_code',
-            $resource->getCurrencyCode());
-            $this->addDataForResource($resource, 'Checkout_completed_at', $resource->getCheckoutCompletedAt());
-            $this->addDataForResource($resource, 'Payment_state', $resource->getPaymentState());
-            $this->addDataForResource($resource, 'Checkout_state', $resource->getCheckoutState());
-            $this->addDataForResource($resource, 'Shipping_state',
-            $resource->getShippingState());
-            $this->addDataForResource($resource, 'Token_value',
-            $resource->getTokenValue());
-            $this->addDataForResource($resource, 'Customer_ip',
-            $resource->getCustomerIp());
-            $this->addDataForResource($resource, 'Notes',
-            $resource->getNotes());
         }
+    }
+
+    private function addGeneralData(OrderInterface $resource): void
+    {
+        $this->addDataForResource($resource, 'Currency_code', $resource->getCurrencyCode());
+        $this->addDataForResource($resource, 'Checkout_completed_at', $resource->getCheckoutCompletedAt());
+        $this->addDataForResource($resource, 'Payment_state', $resource->getPaymentState());
+        $this->addDataForResource($resource, 'Checkout_state', $resource->getCheckoutState());
+        $this->addDataForResource($resource, 'Shipping_state', $resource->getShippingState());
+        $this->addDataForResource($resource, 'Token_value', $resource->getTokenValue());
+        $this->addDataForResource($resource, 'Customer_ip', $resource->getCustomerIp());
+        $this->addDataForResource($resource, 'Notes', $resource->getNotes());
     }
 
     private function addCustomerData(OrderInterface $resource): void
