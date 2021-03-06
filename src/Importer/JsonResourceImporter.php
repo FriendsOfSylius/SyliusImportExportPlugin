b703b221023db41ea9591c121929e616a86a1731
Gogopex <gogopex@gmail.com>
2021-03-06 23:38:11 +0100

    updated the treebuilder in Configuration.php and replaced ObjectMapper with EntityManagerInterface to update bundle for sylius 1.9

--- src/Importer/JsonResourceImporter.php
+++ src/Importer/JsonResourceImporter.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace FriendsOfSylius\SyliusImportExportPlugin\Importer;
 
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use FriendsOfSylius\SyliusImportExportPlugin\Exception\ImporterException;
 use FriendsOfSylius\SyliusImportExportPlugin\Processor\ResourceProcessorInterface;
 
 final class JsonResourceImporter extends ResourceImporter implements SingleDataArrayImporterInterface
 {
     public function __construct(
-        ObjectManager $objectManager,
+        EntityManagerInterface $objectManager,
         ResourceProcessorInterface $resourceProcessor,
         ImportResultLoggerInterface $importerResult,
         int $batchSize,
