094f920b5b457bb61e6b7db1628a6053a0cf0ac6
Valentin Silvestre <vsilvestre.pro@gmail.com>
2022-10-10 09:34:30 +0200

    Test build fix for master

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -6,7 +6,7 @@ $bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true], // no change commit
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sylius\Bundle\OrderBundle\SyliusOrderBundle::class => ['all' => true],
